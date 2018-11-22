4d345138c5b1114a385ed0f4a8550f34876395c0
JoaquinLeonArg <leonjoaquin77@gmail.com>
2018-11-22 20:22:09 -0300

    Botones funcionando

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
   $('#disp_scr').hide();
   $('#reglas_scr').hide();
 });
+
+$('#boton_1').click(function(){
+  $('#sensor_titulo').text("Sensor Temp_1");
+  $('#sensor_valor').text("25°C");
+});
+
+$('#boton_2').click(function(){
+  $('#sensor_titulo').text("Sensor Luz_1");
+  $('#sensor_valor').text("100 l");
+});
+
+$('#boton_3').click(function(){
+  $('#sensor_titulo').text("Sensor Sonido_1");
+  $('#sensor_valor').text("10 db");
+});
