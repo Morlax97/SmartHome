a3b6d7e61c570571f472f090008c03a8d7ae806a
BuildTools <unconfigured@null.spigotmc.org>
2018-11-22 19:11:07 -0300

    Menú principal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.tabs').tabs();
-    $("#disp_scr").hide();
-    $("#reglas_scr").hide();
-  });
+  $('.tabs').tabs();
+  $('#disp_scr').hide();
+  $('#reglas_scr').hide();
+});
