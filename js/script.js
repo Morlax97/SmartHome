f0f8ea25d10ef0290fae534b5410fa4a1b2526d8
BuildTools <unconfigured@null.spigotmc.org>
2018-11-23 14:07:54 -0300

    test

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 $(document).ready(function(){
   $('.tabs').tabs();
   $('#sensor_temp').hide();
-  $('#sensor_luz').hide();
+  $('#sensor_luz1').hide();
+  $('#sensor_luz2').hide();
   $('#sensor_sonido').hide();
 });
 
 $('#sensor_temp_btn').click(function(){
   $('#sensor_temp').show();
-  $('#sensor_luz').hide();
+  $('#sensor_luz1').hide();
+  $('#sensor_luz2').hide();
   $('#sensor_sonido').hide();
 });
 
 $('#sensor_luz_btn').click(function(){
   $('#sensor_temp').hide();
-  $('#sensor_luz').show();
+  $('#sensor_luz1').show();
+  $('#sensor_luz2').hide();
   $('#sensor_sonido').hide();
 });
 
 $('#sensor_sonido_btn').click(function(){
   $('#sensor_temp').hide();
-  $('#sensor_luz').hide();
+  $('#sensor_luz1').hide();
   $('#sensor_sonido').show();
+  $('#sensor_luz2').hide();
 });
