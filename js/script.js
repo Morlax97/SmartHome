74c3dde824b289437bae56bbc29a76d9937d725e
BuildTools <unconfigured@null.spigotmc.org>
2018-11-23 14:25:54 -0300

    actuadores

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
   $('#sensor_luz1').hide();
   $('#sensor_luz2').hide();
   $('#sensor_sonido').hide();
+  $('#actuador_lampara').hide();
+  $('#actuador_alarma').hide();
 });
 
 $('#sensor_temp_btn').click(function(){
@@ -11,6 +13,8 @@ $('#sensor_temp_btn').click(function(){
   $('#sensor_luz1').hide();
   $('#sensor_luz2').hide();
   $('#sensor_sonido').hide();
+  $('#actuador_lampara').hide();
+  $('#actuador_alarma').hide();
 });
 
 $('#sensor_luz1_btn').click(function(){
@@ -18,6 +22,8 @@ $('#sensor_luz1_btn').click(function(){
   $('#sensor_luz1').show();
   $('#sensor_luz2').hide();
   $('#sensor_sonido').hide();
+  $('#actuador_lampara').hide();
+  $('#actuador_alarma').hide();
 });
 
 $('#sensor_luz2_btn').click(function(){
@@ -25,12 +31,33 @@ $('#sensor_luz2_btn').click(function(){
   $('#sensor_luz2').show();
   $('#sensor_luz1').hide();
   $('#sensor_sonido').hide();
+  $('#actuador_lampara').hide();
+  $('#actuador_alarma').hide();
 });
-
 
 $('#sensor_sonido_btn').click(function(){
   $('#sensor_temp').hide();
   $('#sensor_luz1').hide();
   $('#sensor_sonido').show();
   $('#sensor_luz2').hide();
+  $('#actuador_lampara').hide();
+  $('#actuador_alarma').hide();
+});
+
+$('#actuador_lamp_btn').click(function(){
+  $('#sensor_temp').hide();
+  $('#sensor_luz1').hide();
+  $('#sensor_sonido').hide();
+  $('#sensor_luz2').hide();
+  $('#actuador_lampara').show();
+  $('#actuador_alarma').hide();
+});
+
+$('#actuador_alarm_btn').click(function(){
+  $('#sensor_temp').hide();
+  $('#sensor_luz1').hide();
+  $('#sensor_sonido').hide();
+  $('#sensor_luz2').hide();
+  $('#actuador_lampara').hide();
+  $('#actuador_alarma').show();
 });
