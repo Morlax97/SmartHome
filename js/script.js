54c08212b2d34bd4ec8320af8819d746b651d7d6
BuildTools <unconfigured@null.spigotmc.org>
2018-11-23 14:17:41 -0300

    actuadores

--- js/script.js
+++ js/script.js
@@ -13,12 +13,20 @@ $('#sensor_temp_btn').click(function(){
   $('#sensor_sonido').hide();
 });
 
-$('#sensor_luz_btn').click(function(){
+$('#sensor_luz1_btn').click(function(){
   $('#sensor_temp').hide();
   $('#sensor_luz1').show();
   $('#sensor_luz2').hide();
   $('#sensor_sonido').hide();
 });
+
+$('#sensor_luz2_btn').click(function(){
+  $('#sensor_temp').hide();
+  $('#sensor_luz2').show();
+  $('#sensor_luz1').hide();
+  $('#sensor_sonido').hide();
+});
+
 
 $('#sensor_sonido_btn').click(function(){
   $('#sensor_temp').hide();
