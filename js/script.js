b85c2a75b984ad85de6ba19d97de083d5ac50958
BuildTools <unconfigured@null.spigotmc.org>
2018-11-22 21:47:54 -0300

    sensor sonido

--- js/script.js
+++ js/script.js
@@ -2,19 +2,23 @@ $(document).ready(function(){
   $('.tabs').tabs();
   $('#sensor_temp').hide();
   $('#sensor_luz').hide();
+  $('#sensor_sonido').hide();
 });
 
 $('#sensor_temp_btn').click(function(){
   $('#sensor_temp').show();
   $('#sensor_luz').hide();
+  $('#sensor_sonido').hide();
 });
 
 $('#sensor_luz_btn').click(function(){
   $('#sensor_temp').hide();
   $('#sensor_luz').show();
+  $('#sensor_sonido').hide();
 });
 
-$('#boton_3').click(function(){
-  $('#sensor_titulo').text("Sensor Sonido_1");
-  $('#sensor_valor').text("10 db");
+$('#sensor_sonido_btn').click(function(){
+  $('#sensor_temp').hide();
+  $('#sensor_luz').hide();
+  $('#sensor_sonido').show();
 });
